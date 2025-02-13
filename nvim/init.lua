0f2fee58ac527144ec7654453f0e54d074bb2c3e
metz <daryljmetzler@gmail.com>
2025-02-12 17:36:45 -0800

    Treesitter install
    Still dont have a good sense of what this is for.

--- nvim/init.lua
+++ nvim/init.lua
@@ -91,6 +91,19 @@ require("lazy").setup({
       pcall(require('telescope').load_extension, 'fzf')
     end
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+  },
 })
 
 vim.api.nvim_set_keymap("n", "<Leader>f", ":NvimTreeToggle<cr>", {silent = true, noremap = true})
