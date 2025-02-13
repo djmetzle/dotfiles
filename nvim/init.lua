0e8ae9fd4a76759096634e116b7b3cd4e2d3f601
metz <daryljmetzler@gmail.com>
2025-02-12 17:03:13 -0800

    Find git files in project

--- nvim/init.lua
+++ nvim/init.lua
@@ -100,6 +100,7 @@ vim.api.nvim_set_keymap("n", "<Leader><Space>", [[:%s/\s\+$//e<CR>:nohl<CR>]], {
 --- Telescope keybinds
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fp', builtin.git_files, { desc = 'Telescope git files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
