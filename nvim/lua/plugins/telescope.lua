c892c263e6f5edd77d3b6c706daf2dfbf626b944
metz <daryljmetzler@gmail.com>
2025-02-16 20:26:26 -0800

    Add dev icons for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
       'nvim-telescope/telescope-fzf-native.nvim',
       build = 'make',
     },
+    'nvim-tree/nvim-web-devicons',
   },
   config = function()
     pcall(require('telescope').load_extension, 'fzf')
