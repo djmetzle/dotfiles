60911a5280dda19410f02a275f14f56721e16aeb
metz <daryljmetzler@gmail.com>
2025-02-16 16:59:31 -0800

    Extract module for telescope install

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,7 @@ require("lazyinstall")
 
 require("lazy").setup({
   checker = { enabled = true },
+  {import = "plugins"},
   'tpope/vim-sleuth',
   {
     "nvim-tree/nvim-tree.lua",
@@ -38,19 +39,6 @@ require("lazy").setup({
     opts = {},
   },
   {
-    'nvim-telescope/telescope.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-      },
-    },
-    config = function()
-      pcall(require('telescope').load_extension, 'fzf')
-    end
-  },
-  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     opts = {
@@ -72,13 +60,5 @@ vim.api.nvim_set_keymap("n", "<Leader>f", ":NvimTreeToggle<cr>", {silent = true,
 vim.api.nvim_set_keymap("n", "<Leader><Space>", [[:%s/\s\+$//e<CR>:nohl<CR>]], {noremap = true})
 
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-
---- Telescope keybinds
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fp', builtin.git_files, { desc = 'Telescope git files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
