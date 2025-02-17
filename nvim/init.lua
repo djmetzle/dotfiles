0eefb001ce138d2933113040a7f7a3704849f92d
metz <daryljmetzler@gmail.com>
2025-02-16 20:16:12 -0800

    Fix treesitter include

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,6 +8,7 @@ require("lazy").setup({
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    main = 'nvim-treesitter.configs',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'ruby', 'php', 'astro' },
       auto_install = true,
