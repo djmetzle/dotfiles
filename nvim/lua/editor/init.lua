e9a16e004eb86c6fc6f90fd123d943bfda6e1496
metz <daryljmetzler@gmail.com>
2025-02-17 09:36:33 -0800

    Indent with spaces

--- nvim/lua/editor/init.lua
+++ nvim/lua/editor/init.lua
@@ -4,13 +4,15 @@ vim.opt.relativenumber = true
 vim.opt.showmode = false
 
 vim.opt.breakindent = true
+vim.opt.tablestop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 vim.opt.undofile = true
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
-vim.opt.breakindent = true
 
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
