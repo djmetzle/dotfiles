79e6c4ae322dbe6dd0b5f935494c7dc98be282f6
metz <daryljmetzler@gmail.com>
2025-02-27 23:37:39 -0800

    Add in nvim-cmp mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -99,6 +99,13 @@ require("lazy").setup({
             luasnip.lsp_expand(args.body)
           end,
         },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
         completion = { completeopt = 'menu,menuone,noinsert' },
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
