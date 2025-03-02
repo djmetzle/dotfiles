d5eafcf8651431b3b2844b9e2c658d10fe69859e
metz <daryljmetzler@gmail.com>
2025-03-02 10:49:35 -0800

    LuaSnip config

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,7 +2,20 @@ return {
   {
     'hrsh7th/nvim-cmp',
     dependencies = {
-      'L3MON4D3/LuaSnip',
+      {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
+        },
+      },
       'saadparwaiz1/cmp_luasnip',
       'rafamadriz/friendly-snippets',
       'ray-x/cmp-treesitter',
