9c39079bc957edfeb6386c2e74b0ccd0745123b1
metz <daryljmetzler@gmail.com>
2025-03-02 10:42:59 -0800

    Add friendly-snippets

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
+      'rafamadriz/friendly-snippets',
       'ray-x/cmp-treesitter',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-buffer',
