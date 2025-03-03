35ba8db1e12cb970736ba4303c8ee1b3f17f0283
metz <daryljmetzler@gmail.com>
2025-03-02 18:44:13 -0800

    Add Conform formatting

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,27 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {},
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end)
+	end,
+}
