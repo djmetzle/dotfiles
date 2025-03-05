c19a0628db596232d46a760e0514687dcf6dc816
metz <daryljmetzler@gmail.com>
2025-03-04 22:33:34 -0800

    Set some simple editor options

--- nvim/lua/editor/init.lua
+++ nvim/lua/editor/init.lua
@@ -3,11 +3,16 @@ vim.opt.relativenumber = true
 
 vim.opt.showmode = false
 
+vim.opt.spell = true
+
 vim.opt.breakindent = true
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+
+vim.opt.colorcolumn = "80"
 
 vim.opt.undofile = true
 
