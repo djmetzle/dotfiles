6b46d5533600ecbe382b88a43bb8390039d4b020
metz <daryljmetzler@gmail.com>
2025-03-04 17:42:47 -0800

    Better buffers key

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -15,7 +15,8 @@ return {
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
     vim.keymap.set("n", "<leader>fp", builtin.git_files, { desc = "Telescope git files" })
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+    vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "telescope buffers" })
   end,
 }
