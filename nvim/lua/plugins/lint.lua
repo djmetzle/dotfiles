0683263ee394862c6a709c975872a868e2372b8c
metz <daryljmetzler@gmail.com>
2025-03-31 17:30:26 -0700

    Add linter plugin

---
+++ nvim/lua/plugins/lint.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+        dockerfile = { "hadolint" },
+        markdown = { "vale" },
+        ruby = { "rubocop" },
+        rust = { "clippy" },
+        sh = { "shellcheck" },
+        terraform = { "tflint" },
+        text = { "vale" },
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          if vim.opt_local.modifiable:get() then
+            lint.try_lint()
+          end
+        end,
+      })
+    end,
+  },
+}
