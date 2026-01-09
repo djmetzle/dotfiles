970f943d1c447b527a7d8b3d45b77458b3de4729
Daryl Metzler <daryl@ifixit.com>
2026-01-09 12:26:49 -0800

    Add claude plugin

---
+++ nvim/lua/plugins/claude-code.lua
@@ -0,0 +1,15 @@
+return {
+  "greggh/claude-code.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    require("claude-code").setup({
+      window = {
+        split_ratio = 0.5,
+      },
+    })
+
+    vim.keymap.set('n', '<leader>cc', '<cmd>ClaudeCode<CR>', { desc = 'Toggle Claude Code' })
+  end
+}
