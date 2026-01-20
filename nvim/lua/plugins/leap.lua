fe558c82d51d7748b2790670e9af65ffce27d977
Daryl Metzler <djmetzle@ncsu.edu>
2026-01-20 13:42:04 -0800

    Fix leap keybinds

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -2,8 +2,9 @@ return {
   {
     "ggandor/leap.nvim",
     config = function()
-      local leap = require("leap")
-      leap.add_default_mappings(true)
+      vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+      vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+      vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
     end,
   },
 }
